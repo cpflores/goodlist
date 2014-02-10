25be208239f266311c39c14e09f4f1c858cbce36
Chadwick Flores <chadwickflores@gmail.com>
2014-02-10 14:37:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goodlist::Application.config.secret_key_base = 'a896665520b8f1e31d85bf5e8a6def19a6f21aa66a58c43499984c126aea3307c289630b3c46c839c218e052e3c5f8e953056380d3ab313641ad43bb02b41ab7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goodlist::Application.config.session_store :cookie_store, key: '_goodlist_session'
