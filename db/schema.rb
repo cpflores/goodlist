b363528454a7b9973dcccc4fa9dd1262a93e8c61
Chadwick Flores <chadwickflores@gmail.com>
2014-02-10 23:08:32 -0600

    include AddImageToPlaces via image link migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211041633) do
+ActiveRecord::Schema.define(version: 20140211044912) do
 
   create_table "places", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140211041633) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "kind"
+    t.string   "image"
   end
 
 end
