a0cb8ed292cd1352ec14086c219a8fe1eeb5b857
Chadwick Flores <chadwickflores@gmail.com>
2014-02-10 22:43:01 -0600

    add AddKindToPlaces migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140210221040) do
+ActiveRecord::Schema.define(version: 20140211041633) do
 
   create_table "places", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "kind"
   end
 
 end
