706f6fcc9d376ea8237acb0af388d449fa56432e
Chadwick Flores <chadwickflores@gmail.com>
2014-04-18 08:36:45 -0500

    assocatiated places and users, user signup/login to nav

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417024633) do
+ActiveRecord::Schema.define(version: 20140418132228) do
 
   create_table "places", force: true do |t|
     t.string   "title"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20140417024633) do
     t.datetime "updated_at"
     t.string   "kind"
     t.string   "image"
+    t.integer  "user_id"
   end
+
+  add_index "places", ["user_id"], name: "index_places_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
